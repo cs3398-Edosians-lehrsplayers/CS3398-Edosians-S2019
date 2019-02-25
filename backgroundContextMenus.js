8b2de7d6402971d681977bf8132dd3e94ab92696
WarrenMcCray <47012252+WarrenMcCray@users.noreply.github.com>
2019-02-24 20:17:23 -0600

    Testing Multiple Context Menus
    Taking an array of strings and making dummy context menu items for each one.

--- backgroundContextMenus.js
+++ backgroundContextMenus.js
@@ -1,7 +1,16 @@
+var CONTEXT_MENU_ITEMS = [
+    'Test Item 1',
+    'Another Item',
+    'WE IN BUSINESS, BOIS'
+];
+
+
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.contextMenus.create({
-    "id": "testContextMenu",
-    "title": "Test Context Menu Item",
-    "contexts": ["all"]
-  });      
+  CONTEXT_MENU_ITEMS.forEach(function(item) {
+    chrome.contextMenus.create({
+      "id": "_" + item,
+      "title": item,
+      "contexts": ["all"]
+    });   
+  });   
 });
