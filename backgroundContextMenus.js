e3ad74b206f6cbc2138c0136de9f57bd7ae88296
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-03-05 02:06:14 -0600

    update

--- backgroundContextMenus.js
+++ backgroundContextMenus.js
@@ -1,7 +1,12 @@
+var str = ["spencer", "john", "jaysen"];
+
+var i = Math.floor(Math.random() * 3);
+
 var CONTEXT_MENU_ITEMS = [
     'Test Item 1',
     'Another Item',
-    'WE IN BUSINESS, BOIS'
+    'WE IN BUSINESS, BOIS',
+    str[i]
 ];
 
 
@@ -26,12 +31,18 @@ chrome.runtime.onInstalled.addListener(function() {
   // When the icon is full-color, users can click it to view popup.html
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostEquals: 'developer.chrome.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
+  });
+
+  // test to be used with options.js
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
   });
 
 });
