52c7d09af78477764206987bc2d023a442ff1f86
WarrenMcCray <47012252+WarrenMcCray@users.noreply.github.com>
2019-03-07 17:48:33 -0600

    Adding getting kanji grade through context menu functionality
    Selecting text with kanji in it and selecting the "Check for kanji" context menu item will, in most cases, print the characters and the grade to console.

--- backgroundContextMenus.js
+++ backgroundContextMenus.js
@@ -25,7 +25,13 @@ chrome.runtime.onInstalled.addListener(function() {
     "contexts": ["selection"],
     "onclick": googleIt
   });
-
+  
+  chrome.contextMenus.create({
+     "id": "isKanji",
+     "title": "Check for kanji",
+     "contexts": ["selection"],
+     "onclick": checkForKanji
+  });
   // This will alow the browser to show a full-color page icon in the browser
   // toolbar when users navigate to a URL containing "developer.chrome.com". 
   // When the icon is full-color, users can click it to view popup.html
@@ -50,4 +56,26 @@ chrome.runtime.onInstalled.addListener(function() {
 function googleIt(e) {
   var newURL = "http://www.google.com/search?q=" + e.selectionText;
   chrome.tabs.create({ url: newURL });
+}
+
+// Checks selected text for kanji. If found, prints character and grade to console.
+function checkForKanji(e) {
+    var str = e.selectionText;
+    var i;
+    for(i = 0; i < str.length; i++) {
+        if(str.charCodeAt(i) >= 0x4E00 && str.charCodeAt(i) <= 0x9FAF) {
+            //var json = $.getJSON("https://kanjialive-api.p.rapidapi.com/api/public/kanji/" + str.charAt(i));
+            $.ajax({
+                url: "https://kanjialive-api.p.mashape.com/api/public/kanji/" + str.charAt(i),
+                dataType: "JSON",
+                headers: {
+                    "X-RapidAPI-Key": "06dd6c2851msh7d0e21955b66957p16986cjsn4b054ebb5f8c"
+                }
+            }).done(function(data) {
+                console.log(data.kanji.character + ": Grade " + data.references.grade);
+            });
+            //console.log(str.charAt(i) + typeof json);
+            
+        }
+    }
 }
