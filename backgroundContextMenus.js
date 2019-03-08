9ded0413124c80fef33a1d0f51860a4f29b681cf
John Pappas <44303481+jaydeepappas@users.noreply.github.com>
2019-03-08 13:55:39 -0600

    Random website button implimented

--- backgroundContextMenus.js
+++ backgroundContextMenus.js
@@ -9,6 +9,51 @@ var CONTEXT_MENU_ITEMS = [
     str[i]
 ];
 
+var links = [
+	"wikipedia.org/",
+	"stackoverflow.com/",
+	"gizmodo.com/",
+	"geeksforgeeks.org/",
+	"nytimes.com/",
+	"theguardian.com/",
+	"narratively.com/",
+	"wired.com/",
+	"grantland.com/",
+	"bloomberg.com/",
+	"nymag.com/",
+	"laphamsquarterly.org/",
+	"theatlantic.com/",
+	"macrumors.com/",
+	"digitaltrends.com/",
+	"pbs.org/",
+	"nationalpost.com/",
+	"businessinsider.com/",
+	"motherjones.com/",
+	"hthenextweb.com/",
+	"espn.com/",
+	"venturebeat.com/",
+	"nybooks.com/",
+	"theringer.com/",
+	"techradar.com/",
+	"smithsonianmag.com/",
+	"theamericanscholar.org/",
+	"firstpost.com/",
+	"arktimes.com/",
+	"eater.com/",
+	"mashable.com/",
+	"beltmag.com/",
+	"engadget.com/",
+	"synacor.com/",
+	"thecut.com/",
+	"droid-life.com/",
+	"newyorker.com/",
+	"slashgear.com/",
+	"gq.com/",
+	"atlantamagazine.com/",
+	"medium.com/",
+	"techcrunch.com/",
+	"theverge.com/"]
+
 
 chrome.runtime.onInstalled.addListener(function() {
   CONTEXT_MENU_ITEMS.forEach(function(item) {
@@ -24,7 +69,14 @@ chrome.runtime.onInstalled.addListener(function() {
     "title": "Google \'%s\'",
     "contexts": ["selection"],
     "onclick": googleIt
-  });
+	});
+
+	chrome.contextMenus.create({
+		"id": "Random Site",
+		"title": "Click for random site!",
+		"contexts": ["all"],
+		"onclick": openSite
+	});
 
   // This will alow the browser to show a full-color page icon in the browser
   // toolbar when users navigate to a URL containing "developer.chrome.com". 
@@ -51,3 +103,12 @@ function googleIt(e) {
   var newURL = "http://www.google.com/search?q=" + e.selectionText;
   chrome.tabs.create({ url: newURL });
 }
+
+var openSite = function () {
+	var randIdx = Math.random() * links.length;
+	randIdx = parseInt(randIdx, 10);
+	var link = 'http://' + links[randIdx];
+	var win = window.open(link, '_blank');
+	win.focus();
+
+};
