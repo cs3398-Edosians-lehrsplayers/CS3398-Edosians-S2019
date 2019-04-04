5cca21037ff2077b6991dd3723cbc9c46d7bf43c
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-04-04 12:39:47 -0500

    Task1: Notfications

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,7 +2,12 @@
 
 
 
-var urlmenu = document.getElementById( 'topic' );
- urlmenu.onchange = function() {
-      window.open( this.options[ this.selectedIndex ].value );
- };
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('popup.html', {
+    id: 'main',
+    bounds: { width: 620, height: 500 }
+  });
+});
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+  console.log("Got an alarm!", alarm);
+});
