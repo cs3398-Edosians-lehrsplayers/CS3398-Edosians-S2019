875ab246d8649ccbb78f07b54afc2e2f30651a35
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-04-08 01:26:12 -0500

    Dropdown menu is working.
    Shooting to have user input done by tomorrow.

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,30 @@
-//To be updated, just to get UI started
+function getOption(){
+    
+}
 
 
-/* chrome.alarms.onAlarm.addListener(function( alarm ) {
-  console.log("Got an alarm!", alarm);
-}); */
+function addOption(){
+	var select = document.getElementById("jumpmenu");
+    select.options[select.options.length] = new Option('New Element', '0', false, false);
+}
+
+function removeOption(){
+   
+}
+
+function removeAllOptions(){
+    var select = document.getElementById("jumpmenu");
+    select.removeAllOptions;
+}
+
+
+document.getElementById("jumpmenu").onchange = function() {myFunction()};
+
+
+
+
+function myFunction() {
+	var x = document.getElementById("jumpmenu").value;
+	var win = window.open(x, '_blank');
+	win.focus();
+}
